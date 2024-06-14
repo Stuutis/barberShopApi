dde52a0d12ca650d86965b44696ba9d35e7f5a16
Christian <christianbutkevicis@gmail.com>
2024-06-13 22:33:37 -0300

    feito rota de login

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,8 +2,17 @@ import { Prisma, PrismaClient } from "@prisma/client";
 import { createUserSchema } from "../schemas/userSchema";
 import { FastifyRequest, FastifyReply } from "fastify";
 import { z } from "zod";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
+
+const JWT_SECRET = "pkL!çEfb423$";
+
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
 
 export const getUsers = async (
   request: FastifyRequest,
@@ -22,12 +31,14 @@ export const createUser = async (
       request.body
     );
 
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     await prisma.user.create({
       data: {
         firstName,
         lastName,
         email,
-        password,
+        password: hashedPassword,
       },
     });
     reply.header("Access-Control-Allow-Origin", "http://localhost:5173");
@@ -43,6 +54,41 @@ export const createUser = async (
           return reply.status(409).send({ error: "Email já existe." });
         }
       }
+    }
+    return reply.status(500).send({ error: "Internal Server Error" });
+  }
+};
+
+export const loginUser = async (
+  request: FastifyRequest,
+  reply: FastifyReply
+) => {
+  try {
+    const { email, password } = loginSchema.parse(request.body);
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    if (!user) {
+      console.log("Usuário não encontrado");
+      return reply.status(404).send({ error: "Usuário não encontrado" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      console.log("Senha incorreta");
+      return reply.status(401).send({ error: "Senha incorreta" });
+    }
+
+    const token = jwt.sign({ userId: user.id }, JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    reply.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    return reply.status(200).send({ token });
+  } catch (error) {
+    console.error("Erro no login:", error);
+    if (error instanceof z.ZodError) {
+      return reply.status(400).send({ error: error.errors });
     }
     return reply.status(500).send({ error: "Internal Server Error" });
   }
--- src/routes/userRotues.ts
+++ src/routes/userRotues.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance } from "fastify";
-import { getUsers, createUser } from "../controllers/userController";
+import { getUsers, createUser, loginUser } from "../controllers/userController";
 
 export const userRoutes = (app: FastifyInstance) => {
   app.get("/users", getUsers);
   app.post("/users", createUser);
+  app.post("/login", loginUser);
 };
